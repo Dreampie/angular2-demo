5732d58d58c44d0a656886f0d6b3752390c90ef8
Dreampie <Dreampie@outlook.com>
2016-04-22 17:57:55 +0800

    Initial commit

---
+++ build/webpack.dev.config.js
@@ -0,0 +1,51 @@
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const version = require("../package.json").version;
+const banner =
+  "/**\n" +
+  " * angular-demo v" + version + "\n" +
+  " * https://github.com/Dreampie/angular2-demo\n" +
+  " * Released under the MIT License.\n" +
+  " */\n";
+
+module.exports = [
+  {
+    devServer: {
+      contentBase: 'dist',
+      hot: true,
+      historyApiFallback: true,
+      inline: true,
+      port: 80
+    },
+    devtool: 'source-map',
+    debug: true,
+    entry: {
+      app: "./src/main"
+    },
+    output: {
+      publicPath: "/",
+      path: "./dist",
+      filename: "[name].min.js"
+    },
+    module: {
+      loaders: [
+        {test: /\.less/, loader: 'style!css!less'},
+        {test: /\.html$/, loader: "html"},
+        {test: /\.ts/, loader: 'ts'},
+        {test: /\.(eot|svg|ttf)$/, loader: "file"},
+        {test: /\.(png|jpg|gif)$/, loader: "url"},
+        {test: /\.(woff|woff2)$/, loader: "url?limit=10000&minetype=application/font-woff"}
+      ]
+    },
+    resolve: {
+      modulesDirectories: ['node_modules'],
+      extensions: ['', '.html', '.less', '.ts']
+    },
+    plugins: [
+      new webpack.BannerPlugin(banner, {raw: true}),
+      new HtmlWebpackPlugin({inject: false, template: 'src/index.html'}),
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoErrorsPlugin()
+    ]
+  }
+];
---
+++ src/app.component.js
@@ -0,0 +1,16 @@
+"use strict";
+var core_1 = require("angular2/core");
+var AppComponent = (function () {
+    function AppComponent() {
+    }
+    AppComponent = __decorate([
+        core_1.Component({
+            selector: 'app',
+            template: '<h1>My First Angular 2 App</h1>'
+        }), 
+        __metadata('design:paramtypes', [])
+    ], AppComponent);
+    return AppComponent;
+}());
+exports.AppComponent = AppComponent;
+//# sourceMappingURL=app.component.js.map
