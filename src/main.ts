030a220ea34c9ef199b9dd4b3517530bf4a955e1
Dreampie <Dreampie@outlook.com>
2016-04-23 19:54:13 +0800

    Uncaught ReferenceError: __decorate is not defined

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
-import 'reflect-metadata';
 import 'es6-shim';
+import 'es6-promise';
+import 'zone.js/dist/zone';
+import 'reflect-metadata';
 import {bootstrap} from "angular2/platform/browser";
 import {AppComponent} from "./app.component";
 
